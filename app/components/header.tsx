adafcab42be0aaf654794dd3fd0c9acb8f4d1708
Ryan Yogan <ryanyogan@gmail.com>
2024-02-17 18:10:48 -0600

    feat // springy yay

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,12 +1,13 @@
+import { Link } from "@remix-run/react";
 import { SiRevoltdotchat } from "react-icons/si";
 import { Nav } from "./nav";
 
 export function Header() {
   return (
     <header className="mx-auto flex w-full max-w-7xl flex-wrap items-center justify-between border-b border-gray-100 bg-background p-[2em] font-sans font-bold text-text-primary backdrop-blur-[100px] dark:border-gray-800 dark:bg-d-background dark:text-d-text-primary">
-      <span className="text-4xl mr-16">
-        <SiRevoltdotchat className="text-slate-800" />
-      </span>
+      <Link to="/" prefetch="intent" className="text-4xl mr-16">
+        <SiRevoltdotchat className="text-slate-800 hover:text-slate-500" />
+      </Link>
       <Nav />
     </header>
   );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,10 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { motion } from "framer-motion";
 
 import { getPosts } from "~/.server/posts";
 import { Post } from "~/components/post";
+import { containerVariants, textVariants } from "~/utils/animation-config";
 
 export const loader = async () => {
   const posts = await getPosts();
@@ -14,16 +16,23 @@ export default function Index() {
 
   return (
     <div className="flex-1 p-10 grid grid-cols-1 lg:grid-cols-2 gap-16 sm:place-items-center">
-      <div className="space-y-8">
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={containerVariants}
+        className="space-y-8"
+      >
         <div>
           <h2 className="text-4xl font-bold">Incredible 🤯 Thoughts</h2>
           <p className="text-gray-600 font-light">
             Powered by this spectacular brain.
           </p>
         </div>
+
         <hr />
-        <section>
-          <h3 className="text-xl tracking-wide">✨ THOUGHTS ✨</h3>
+
+        <motion.section variants={textVariants}>
+          <h3 className="text-slate-600 font-light">✨ I like these ones ✨</h3>
           <ul className="mt-4 space-y-8">
             {featuredPosts.map((post) => (
               <li key={post.slug}>
@@ -31,9 +40,15 @@ export default function Index() {
               </li>
             ))}
           </ul>
-        </section>
-      </div>
-      <div className="bg-gradient-to-t from-transparent to-slate-100 p-8 rounded-xl">
+        </motion.section>
+      </motion.div>
+
+      <motion.div
+        variants={textVariants}
+        initial="hidden"
+        animate="visible"
+        className="bg-gradient-to-t max-w-xl from-transparent to-slate-100 shadow-sm p-8 rounded-xl"
+      >
         <h2 className="text-slate-800 text-lg font-bold mb-2">
           Hi, I&apos;m Ryan 👋
         </h2>
@@ -56,7 +71,7 @@ export default function Index() {
             const` 🤷
           </p>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
