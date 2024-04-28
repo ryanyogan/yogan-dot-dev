7a63f9dafcfb5420548ab56379117924fb5be358
Ryan Yogan <ryanyogan@gmail.com>
2024-04-27 21:04:32 -0500

    fixing envs

--- env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="@remix-run/node" />
-/// <reference types="vite/client" />
-
-declare module "virtual:remix/server-build" {
-  import { ServerBuild } from "@remix-run/node";
-  export const routes: ServerBuild["routes"];
-}
